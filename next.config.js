0b73e7da9406971f3fdd93709bedd687be68106f
Floffah <27270386+Floffah@users.noreply.github.com>
2021-10-27 21:05:57 +0000

    bump deps & cool gradient ui on browse

--- next.config.js
+++ next.config.js
@@ -1,20 +1,10 @@
-const withPlugins = require("next-compose-plugins");
-const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
-
-const vanillaExtractPlugin = createVanillaExtractPlugin();
-
-const isdev = process.env.NODE_ENV === "development";
-
-module.exports = withPlugins(
-    [[vanillaExtractPlugin, { short: !isdev, debug: isdev }]],
-    {
-        reactStrictMode: true,
-        webpack5: true,
-        future: {
-            modern: true,
-        },
-        images: {
-            domains: ["twemoji.maxcdn.com"],
-        },
+module.exports = {
+    reactStrictMode: true,
+    webpack5: true,
+    future: {
+        modern: true,
     },
-);
+    images: {
+        domains: ["twemoji.maxcdn.com"],
+    },
+};
--- src/pages/browse/index.tsx
+++ src/pages/browse/index.tsx
@@ -4,13 +4,8 @@ export default function BrowsePage() {
     return (
         <>
             <Navbar />
-            <div className="ml-8">
-                {/* featured */}
-                <div className="mt-16">
-                    <h1 className="text-2xl font-bold">Featured</h1>
-                </div>
-
-                <div className="mt-5 text-center">
+            <div className="ml-8 mr-8">
+                <div className="mt-16 py-4 text-center rounded-lg bg-gradient-to-t from-gray-900 to-gray-800">
                     <h1 className="text-2xl">
                         Want to create your own timeline?
                     </h1>
@@ -25,6 +20,11 @@ export default function BrowsePage() {
                         </a>
                         !
                     </p>
+                </div>
+
+                {/* featured */}
+                <div className="mt-5">
+                    <h1 className="text-2xl font-bold">Featured</h1>
                 </div>
             </div>
         </>
